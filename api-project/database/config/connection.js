32440eff95265a599af4a0ba4907641b9f2e26b3
michalMenda <m0583216525@gmail.com>
2025-05-01 17:22:06 +0300

    first commit

---
+++ api-project/database/config/connection.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2/promise');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../server/config/.env') });
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Check if it worked
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database as id ' + connection.threadId);
+});
+module.exports = pool;
